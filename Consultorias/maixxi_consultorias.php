f5444d7d4527d0a7129a7b736f2c883b4f5e4951
gramsch <gramsch@gmail.com>
2016-02-18 18:21:51 -0300

    contenido en bruto de la pagina

---
+++ Consultorias/maixxi_consultorias.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<Meta charset="utf-8">
+	<link href="bootstrap-3.3.6-dist/css/bootstrap.min.css" rel="stylesheet">
+	<link href='Maixxi_consultorias.css' type='text/css' rel='stylesheet'>
+	<title>MAI XXI - Consultores en Ingeniería del Mantenimiento</title>
+	<style type="text/css">
+		center {
+			width: 70%;
+			text-align: center;
+		}
+	</style>
+
+</head>
+<body>
+<?php Include 'header.php'; ?>
+<div class='center'>
+	<div><center>
+	<h3 class='titulo'>TEMAS DE MEJORAMIENTO <br>EN<br>GESTION DEL MANTENIMIENTO</h3>
+	<h4>(RECOMENDADOS PARA SER TOMADOS EN CUENTA EN PROGRAMAS DE MODERNIZACION DE LA FUNCION MANTENIMIENTO)</h4>
+</center><br><br>
+	</div>
+	<h4 class='blue'>Actividades progresivas de mejoramiento de la gestión de mantenimiento:</h4><br>
+	<h4>Etapa de proyecto</h4>
+	<ol>
+		<li>Especificaciones de mantenibilidad en el proyecto, en sus etapas de ingeniería conceptual, básica y de detalle</li><br>
+		<li>Control de la inclusión en el diseño de la mantenibilidad especificada y de su construcción en terreno</li><br>
+		<li>Análisis de fallas potenciales (FMECA) para mejorar el diseño y lograr unaoperación con el máximo de confiabilidad</li><br>
+		<li>Preparación de los planes de mantenimiento preventivo acordes con el análisis de fallas potenciales para iniciar la operacion con 100% de eficacia</li><br>
+		<li>Identificar el modelo de administración de manteniiento que se utilizará y el software de apoyo necesario</li><br>
+	</ol>
+	<h4>Etapa de operación</h4>
+	<ol>
+		<li><a href="">Auditoría de mantenimiento</a>, benchmarking, diagnóstico e identificación de oportunidades y puntos de mejoramiento</li>
+		<li>Sistema de administración del Mantenimiento (SADEM):
+			<ul>
+				<li>Desarrollo o mejoramiento del sistema existente. Definición del modelo estratégico y sus procedimientos administrativos.</li>
+				<li>Implantación de la solución</li>
+			</ul>
+		</li>
+		<li>Análisis cuantitativo y cualifativo de fallas (FMECA) para mejorar la confiabilidad y disponibilidad de la planta, de los servicios y de los procedimientos.</li>
+		<li>Cambio progresivo del mantenimiento reactivo al proactivo (preventivo y predictivo) sobre la base de los resultados del análisis de fallas. Integración operaciones-mantención</li>
+		<li>Medición y mejoramiento de la productividad y rendimiento de mantención.<br><br><center><p class='verde'>Realización de muestreos del trabajo. Planificacion y programacion para el mejor rendimiento de los recursos materiales y de mano de obra. Uso de indicadores de gestión</p></center></li>
+		<li>Especificación, evaluación e implantación de un software de mantencion (CMMS).<br><br><center><p class='verde'>Determinación de la snecesidades de apoyo informático, selección de un paquete del mercado u opción de desarrollo interno.</p></center></li>
+		<li>Análisis de riesgos y vulnerabilidad de las instalaciones, logro de la certeza de funcionamiento, mejoramiento de la mantenibilidad<br><br><center><p class='verde'>Optimización de costos directos y de no disponibilidad, reemplazo económico de equipos, utilización de terceros, contratacion de servicios.</p></center> </li>
+		<li>Aumento de la integración de la producción y el mantenimiento para optimizar globalmente la función productiva de la planta. Análisis de redundancias e inventarios. Integración de operadores y mantenedores hacia una gestión autónoma. Cambio de la Administración del mantenimiento por la administración de la confiabilidad de la planta</li>
+	</ol>
+	<br>
+	<hr>
+	<!-- Segundo bloque -->
+	<h4 class='titulo'>Beneficios que se obtendran mejorando la gestion del Mantenimiento</h3>
+	<ol>
+		<li><h4 class='title-blue'>Aumento de la disponibilidad de las maquinas e instalaciones</h4><br>
+			<ul><li>Menos tiempos de detención, menos fallas, aumento del tiempo de operación: descubrir la fábrica escondida</li></ul>
+			<p class='verde'>Generalmente e produce un aumento de un 10% de las horas operables después de las primeras acciones de análisis.</p>
+		</li>
+		<li><h4 class='title-blue'>aumento de la productividad de la mano de obra</h4>
+			<ul><li>Dsde un 40%, que suele ser lo corriente en cualquier área de mantenimiento en que no se hace gestión de la "productividad"; hasta un 60% que es un valor alcanzable. Esto implica en una cuadrilla de 10 personas hacer el mismo trabajo con 7 o aumentar el trabajo realizado en el equivalente de 3</li>
+			<li>El aumento de la productividad puede redundar en menos contratistas o servicios externos o menos personal temporal.</li></ul>
+		</li>
+		<li><h4 class='title-blue'>Menos uso de materiales y repuestos</h4><ul>
+			<li>Disminuir el stock en, por lo menos un 20%, por medio del camvbio de política de inventario desde mantener u8n stock hacia la compra según necesidad basada en el plan maestro anual de mantenimiento.</li>
+			<li>Disminuir el uso de repuestos por medio de un mejor diagnóstico y conocimiento acabado del funcionamiento de las máquinas. "Menos cambios de repuestos hasta dar con la falla".</li>
+			<li>Aumento de la "vida útil" de componentes y repuestos por medio del mantenimiento preventivo y predictivo que disminuye el deterioro y mide el proceso de desgaste haciendo los cambios en el momento oportuno.</li>
+			<li>Menor costo de mantención del inventario por medio de un sistema mejorado de control que evite las pérdidas por obsolescencia, manejo inadecuado, bajo consumo, etc.</li></ul>
+		</li>
+		<li><h4 class='title-blue'>Disminucion del costo global de mantenimiento</h4><ul>
+			<li>El mejor control que se logra con la implantación o mejoramiento del sistema de administración de mantención redunda en ahorros significativos por menor realización de trabajos duplicados o poco relevantes</li></ul>
+		</li>
+	</ol>
+</div>
+</body>
+</html>
