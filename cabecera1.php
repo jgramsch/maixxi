44b3747f6d84468d62244cf27b5b030b553c8885
jgramsch <jigramsch@uc.cl>
2016-02-18 17:26:50 -0300

    pe ache pes

---
+++ cabecera1.php
@@ -0,0 +1,18 @@
+<div class="jumbotron">
+	<div class="container ">
+		<div class="row">
+			<div class="col-lg-4">
+					<img src="logoent.gif" alt="Logo M.A.I.XXI" style="width:304px;height:228px;">
+			</div>
+			<div class="col-lg-6">
+				<br>
+				<br>
+				<br>
+				<br>
+				<h1>MAIXXI</h1>
+				<h3>Mantenimiento - Administración - Ingeniería</h3>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</div> 
+</div>
---
+++ menu.php
@@ -0,0 +1,26 @@
+<div class="container">
+	<ul class="nav nav-pills nav-justified">
+		<li role="presentation" class="dropdown active">
+	    	<a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+	      		Principal <span class="caret"></span>
+	    	</a>
+	    	<ul class="dropdown-menu">
+	      	<li><a href="#present" class="active">Presentación</a></li>
+	      	<li><a href="#clientes" >Clientes</a></li>
+	      	<li><a href="#cursos" >Cursos</a></li>
+	    	<li><a href="#consultorias" >Consultorias</a></li>
+	    	<li><a href="#servicios" >Servicios</a></li>
+	    	</ul>
+	    </li>
+		<li role="presentation" class="dropdown">
+	    	<a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+	      		Servicios de apoyo a su gestión<span class="caret"></span>
+	    	</a>
+	    	<ul class="dropdown-menu">
+	      		<li><a href="#power1" >Modelo de Gestión del mantenimineto</a></li>
+	      		<li><a href="#power2" >Modelo para el mejoramiento de la gestión</a></li>
+	    	</ul>
+	    </li>
+		<li role="presentation"><a href="#Link_MAIXXI_CAPACiTACIONES">Capacitaciones</a></li>
+	</ul>
+</div>
