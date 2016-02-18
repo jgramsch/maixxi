186beb9956b1342e1efc975ad6df7f1d2aad7143
jgramsch <jigramsch@uc.cl>
2016-02-18 18:50:26 -0300

    todo en php

---
+++ futer.php
@@ -0,0 +1,11 @@
+<footer>
+	<div class="container">
+		<hr>
+
+		<p>	<small>Contacto:</small></p>
+		<p> <small>E-mail: <a href="mailto:maixxi@manquehue.net"><font color="grey">maixxi@manquehue.net</font></a></small></p>
+		<p><small>Fono/Fax: <font color="grey">+56 2 758 8517</font></small></span></p>
+		<p><small>Dirección: <font color="grey">Torres del Paine Poniente 2489, Peñalolen, Santiago, Chile</font></small></span></p>
+				
+	</div>
+</footer>
