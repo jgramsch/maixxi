a56a350e2de2ffceb9b3206fcc9a781325c55925
jgramsch <jigramsch@uc.cl>
2016-02-25 14:13:54 -0300

    mapa creado

--- Powers/powe1.php
+++ Powers/powe1.php
@@ -11,9 +11,17 @@
 	<img src="../Modelo_gestion.png" alt="Modelo Gestion" usemap="#linkspower">
 
 	<map name="linkspower">
-  		<area shape="rect" coords="0,0,82,126" href="sun.htm" alt="Sun">
-  		<area shape="rect" coords="90,58,3" href="mercur.htm" alt="Mercury">
-  		<area shape="rect" coords="124,58,8" href="venus.htm" alt="Venus">
+  		<area shape="rect" coords="18,226,232,340" href="#edereserva" alt="Edereserva">
+  		<area shape="rect" coords="256,142,442,340" href="#parque" alt="Parque">
+  		<area shape="rect" coords="76,606,386,696" href="#inversionreemplazo" alt="inversionreemplazo">
+  		<area shape="rect" coords="738,238,992,312" href="#disponibilidad" alt="Disponibilidad">
+  		<area shape="rect" coords="888,466,1084,530" href="#mantenibilidad" alt="Mantenibilidad">
+  		<area shape="rect" coords="728,542,878,610" href="#prevderiesgos" alt="prevencionderiesgos">
+  		<area shape="rect" coords="1068,328,1140,412" href="#politicas" alt="politicas">
+  		<area shape="rect" coords="1110,156,1232,212" href="#confiabilidad" alt="confiabilidad">
+  		<area shape="rect" coords="1308,148,1530,274" href="#stock" alt="stockderepuestos">
+  		<area shape="rect" coords="1184,394,1428,482" href="#areamanten" alt="AreaMantenimiento">
+  		<area shape="rect" coords="1008,568,1222,674" href="#stockdecom" alt="stockreparable">
 	</map> 
 	<!-- contenido antes del footer-->
 	<?php include '../futer.php' ?>
