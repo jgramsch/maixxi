e97c5343a5a27b5de18c4a1156c54574e9583166
jgramsch <jigramsch@uc.cl>
2016-02-25 13:18:48 -0300

    powershit

---
+++ Powers/powe1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset='utf-8'>
+	<?php include '../head.php' ?>
+</head>
+<body classs='cuerpo'>
+	<?php include '../cabecera1.php' ?>
+	<?php include '../menu.php' ?>
+	<!-- Contenido-->
+	<img src="../Modelo_gestion.png" alt="Modelo Gestion" usemap="#linkspower">
+
+	<map name="linkspower">
+  		<area shape="rect" coords="0,0,82,126" href="sun.htm" alt="Sun">
+  		<area shape="rect" coords="90,58,3" href="mercur.htm" alt="Mercury">
+  		<area shape="rect" coords="124,58,8" href="venus.htm" alt="Venus">
+	</map> 
+	<!-- contenido antes del footer-->
+	<?php include '../futer.php' ?>
+</body>
+</html>
